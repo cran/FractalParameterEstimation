95cad2b413ff87e8e795de8e8be93719672e42ac
Philipp Hermann <philipp.hermann@jku.at>
2019-07-10 13:03:40 +0000

    version 1.1.2

--- R/GSC_seq.R
+++ R/GSC_seq.R
@@ -2,5 +2,5 @@ GSC_seq <- function (p, sierp=TRUE) {
   N <- length(p)
   if (N==1) return(simMatrix(p[1],sierp))
   else  
-    return(kronecker(GSC_seq(p[1:(N-1)]),simMatrix(p[N],sierp)))
+    return(kronecker(GSC_seq(p[1:(N-1)], sierp),simMatrix(p[N],sierp)))
 }
